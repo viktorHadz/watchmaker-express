c3bd0a7c719f89507957d5489b71200c00755ca4
drslowdeath <vikecah@gmail.com>
2025-06-04 20:11:58 +0100

    design-sys slightly darken lightmode

--- watchmaker/src/components/NavHorizontal.vue
+++ watchmaker/src/components/NavHorizontal.vue
@@ -119,15 +119,6 @@ onUnmounted(() => {
 
     <div class="border-brdr/20 dark:border-fg/20 group relative ml-4 border-l pl-4">
       <DarkMode class="hover:text-acc transition duration-200" />
-      <!-- Tooltip for Dark Mode -->
-      <div
-        class="pointer-events-none absolute -bottom-10 left-1/2 -translate-x-1/2 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
-      >
-        <div class="bg-sec text-fg2 rounded-md px-2 py-1 text-xs whitespace-nowrap shadow-lg">
-          Toggle Theme
-          <div class="bg-sec absolute -top-1 left-1/2 h-2 w-2 -translate-x-1/2 rotate-45"></div>
-        </div>
-      </div>
     </div>
   </nav>
 
